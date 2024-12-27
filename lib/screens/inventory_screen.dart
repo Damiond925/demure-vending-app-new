2e4ff74c822a13109cd1c302c2c4da1ebfa0abee
Damiond925 <damiondbradford96@gmail.com>
2024-12-27 20:13:26 +0000

    added inventory screen.dart

---
+++ lib/screens/inventory_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../services/inventory_service.dart';
+
+class InventoryScreen extends StatefulWidget {
+  const InventoryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InventoryScreen> createState() => _InventoryScreenState();
+}
+
+class _InventoryScreenState extends State<InventoryScreen> {
+  final InventoryService _inventoryService = InventoryService();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Example Data - Preloaded Inventory
+    _inventoryService.addItem(InventoryItem(id: '1', name: 'Chips', quantity: 10));
+    _inventoryService.addItem(InventoryItem(id: '2', name: 'Soda', quantity: 20));
+    _inventoryService.addItem(InventoryItem(id: '3', name: 'Candy', quantity: 15));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final inventory = _inventoryService.getInventory();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inventory'),
+      ),
+      body: ListView.builder(
+        itemCount: inventory.length,
+        itemBuilder: (context, index) {
+          final item = inventory[index];
+          return ListTile(
+            title: Text(item.name),
+            subtitle: Text('Quantity: ${item.quantity}'),
+            trailing: IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                // Example function to increase quantity
+                setState(() {
+                  _inventoryService.updateQuantity(item.id, item.quantity + 1);
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
